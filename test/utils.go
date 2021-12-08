98bc44184fd220240a77ec1770e0bab227f6451c
mxBossard <mxbossard@gmail.com>
2021-12-08 07:44:33 +0100

    WIP: implementing resource scanner..

--- test/utils.go
+++ test/utils.go
@@ -35,10 +35,25 @@ func BuildRandTempPath() (filePath string, err error) {
 	return
 }
 
-func MkRandTempDir() (path string) {
-        path, _ = BuildRandTempPath()
-        os.MkdirAll(path, 0755)
-        os.Chdir(path)
+func MkRandTempDir() (path string, err error) {
+        path, err = BuildRandTempPath()
+	if err != nil {
+		return
+	}
+        err = os.MkdirAll(path, 0755)
+        //os.Chdir(path)
+        return
+}
+
+func MkRandSubDir(parentPath string) (path string, err error) {
+	fileName := RandSeq(6)
+	path = filepath.Join(parentPath, fileName)
+	err = os.RemoveAll(path)
+	if err != nil {
+		return
+	}
+        err = os.MkdirAll(path, 0755)
+        //os.Chdir(path)
         return
 }
 
