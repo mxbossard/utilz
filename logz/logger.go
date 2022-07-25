a354857c042d8f54df8c73b7f1d3b57abd3da2a2
mxBossard <mxbossard@protonmail.com>
2022-07-25 08:19:10 +0200

    Add log verbosity filtering

--- logz/logger.go
+++ logz/logger.go
@@ -1,21 +1,24 @@
 package logz
 
 import (
-	"io"
-	"os"
-	"time"
 	"fmt"
-	"strings"
+	"io"
 	"log"
+	"os"
+	"strings"
+	"time"
 
 	"mby.fr/utils/ansi"
 	"mby.fr/utils/format"
 )
 
 const (
+	MinLoggingLevel = 0
+	MaxLoggingLevel = 4
 )
 
 type Logger interface {
+	Filter(loggingLevel int)
 	Trace(string, ...interface{})
 	Debug(string, ...interface{})
 	Info(string, ...interface{})
@@ -25,10 +28,11 @@ type Logger interface {
 }
 
 type Basic struct {
-	out io.Writer
-	name string
+	out            io.Writer
+	name           string
 	colored, timed bool
-	namePadding int
+	namePadding    int
+	loggingLevel   int
 }
 
 func (l Basic) log(kind, color, f string, a ...interface{}) {
@@ -51,26 +55,39 @@ func (l Basic) log(kind, color, f string, a ...interface{}) {
 		prefix += fmt.Sprintf("%-6s ", kind)
 	}
 
-	_, err := fmt.Fprintf(l.out, prefix + f, a...)
+	_, err := fmt.Fprintf(l.out, prefix+f, a...)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
+func (l *Basic) Filter(level int) {
+	l.loggingLevel = level
+}
+
 func (l Basic) Trace(format string, a ...interface{}) {
-	l.log("TRACE", ansi.HilightCyan, format, a...)
+	if l.loggingLevel > 3 {
+		l.log("TRACE", ansi.HilightCyan, format, a...)
+	}
 }
 
 func (l Basic) Debug(format string, a ...interface{}) {
-	l.log("DEBUG", ansi.HilightGreen, format, a...)
+	if l.loggingLevel > 2 {
+		l.log("DEBUG", ansi.HilightGreen, format, a...)
+	}
 }
 
 func (l Basic) Info(format string, a ...interface{}) {
-	l.log("INFO ", ansi.HilightBlue, format, a...)
+	fmt.Printf("logz loggingLevel: %d\n", l.loggingLevel)
+	if l.loggingLevel > 1 {
+		l.log("INFO ", ansi.HilightBlue, format, a...)
+	}
 }
 
 func (l Basic) Warn(format string, a ...interface{}) {
-	l.log("WARN ", ansi.HilightYellow, format, a...)
+	if l.loggingLevel > 0 {
+		l.log("WARN ", ansi.HilightYellow, format, a...)
+	}
 }
 
 func (l Basic) Error(format string, a ...interface{}) {
@@ -82,16 +99,17 @@ func (l Basic) Fatal(format string, a ...interface{}) {
 	os.Exit(1)
 }
 
-func New(out io.Writer, name string, namePadding int, colored, timed bool) Logger {
-	return Basic{
-		out: out, 
-		name: name,
-		namePadding: namePadding,
-		colored: colored,
-		timed: timed,
+func New(out io.Writer, name string, namePadding int, colored, timed bool, level int) Logger {
+	return &Basic{
+		out:          out,
+		name:         name,
+		namePadding:  namePadding,
+		colored:      colored,
+		timed:        timed,
+		loggingLevel: level,
 	}
 }
 
 func Default(name string, padding int) Logger {
-	return New(os.Stdout, name, padding, true, true)
+	return New(os.Stdout, name, padding, true, true, MaxLoggingLevel)
 }
