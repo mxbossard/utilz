19d1070b10c6bebe22877972950e9f10661885ab
mxBossard <mxbossard@gmail.com>
2022-07-26 23:37:40 +0200

    WIP: concurrent helpers

---
+++ concurrent/concurrent_test.go
@@ -0,0 +1,41 @@
+package concurrent
+
+import (
+	//"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	//"github.com/stretchr/testify/require"
+)
+
+var ()
+
+func TestRun(t *testing.T) {
+	expected := []string{"foo", "bar", "baz"}
+	p := func(a string) (string, error) {
+		return a, nil
+	}
+
+	wg, outputs, errors := Run(p, expected...)
+
+	assert.Empty(t, errors)
+	//assert.Empty(t, outputs)
+	assert.True(t, len(outputs) < len(expected))
+	wg.Wait()
+	assert.Empty(t, errors)
+	assert.NotEmpty(t, outputs)
+	assert.Len(t, outputs, len(expected))
+}
+
+func TestRunWaiting(t *testing.T) {
+	expected := []string{"foo", "bar", "baz"}
+	p := func(a string) (string, error) {
+		return a, nil
+	}
+
+	outputs, errors := RunWaiting(p, expected...)
+
+	assert.Empty(t, errors)
+	assert.NotEmpty(t, outputs)
+	assert.Len(t, outputs, len(expected))
+}
