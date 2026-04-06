d5dd55b386728d7bc19712483059f282433e85d9
mxBossard <mxbossard@protonmail.com>
2026-04-06 15:23:32 +0200

    Add some Ptr funcs.

--- ptrz/ptr.go
+++ ptrz/ptr.go
@@ -4,6 +4,22 @@ func BoolPtr(in bool) *bool {
 	return &in
 }
 
+func IntPtr(in int) *int {
+	return &in
+}
+
+func Int8Ptr(in int8) *int8 {
+	return &in
+}
+
+func Int16Ptr(in int16) *int16 {
+	return &in
+}
+
+func Int32Ptr(in int32) *int32 {
+	return &in
+}
+
 func Int64Ptr(in int64) *int64 {
 	return &in
 }
