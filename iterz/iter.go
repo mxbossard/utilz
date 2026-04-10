ed34e69bb88cf19dfd9631526dacdb86e3425447
mxBossard <mxbossard@protonmail.com>
2026-04-10 23:38:47 +0200

    Add iterz pkg.

---
+++ iterz/iter.go
@@ -0,0 +1,333 @@
+package iterz
+
+import (
+	"iter"
+	"sort"
+	"sync"
+
+	"github.com/mxbossard/utilz/collectionz"
+)
+
+func BuildIntSeq(ints ...int) iter.Seq[int] {
+	return func(yield func(int) bool) {
+		for _, i := range ints {
+			if !yield(i) {
+				return
+			}
+		}
+	}
+}
+
+func BuildStringSeq(strings ...string) iter.Seq[string] {
+	return func(yield func(string) bool) {
+		for _, s := range strings {
+			if !yield(s) {
+				return
+			}
+		}
+	}
+}
+
+func Link[K, V any](itk iter.Seq[K], itv iter.Seq[V]) iter.Seq2[K, V] {
+	return func(yield func(K, V) bool) {
+		nextK, stop := iter.Pull(itk)
+		nextV, stop := iter.Pull(itv)
+		defer stop()
+		for {
+			k, ok := nextK()
+			if !ok {
+				return
+			}
+			v, ok := nextV()
+			if !ok {
+				return
+			}
+			if !yield(k, v) {
+				return
+			}
+		}
+	}
+}
+
+// Merge multiple iterators ordering items following compare func.
+// If item A need to be returned before B, compare(A, B) MUST return a negative number
+// If items MAY be returned at indifferent order compare(A, B) must return 0
+// On compare returning 0 keep same iterator in priority.
+// Ideas:
+// - Could preload iterators with a buffer size to enhance oredering
+func Merge[T any](compare func(a, b T) int, its ...iter.Seq[T]) iter.Seq[T] {
+	stop := false
+	chansVal := make(map[int]chan T)
+	chans := &chansVal
+	nextItemsVal := make(map[int]T)
+	nextItems := &nextItemsVal
+	mutex := &sync.Mutex{}
+
+	init := func() {
+		stop = false
+		// m := &sync.Mutex{}
+
+		// Launch goroutines to stream produced datas
+		for pos, it := range its {
+			// Make a buffered chan of size 1
+			c := make(chan T, 1)
+			mutex.Lock()
+			(*chans)[pos] = c
+			mutex.Unlock()
+			go func() {
+				// Continuously push next data
+				defer close(c)
+				for item := range it {
+					c <- item
+					if stop {
+						break
+					}
+				}
+			}()
+		}
+
+		// Init consume first item of each chan
+		for pos, c := range *chans {
+			e, ok := <-c
+			if ok {
+				(*nextItems)[pos] = e
+			} else {
+				mutex.Lock()
+				delete(*chans, pos)
+				mutex.Unlock()
+			}
+		}
+
+		// fmt.Printf("cat paginer initialized chans: %d\n", len(*chans))
+	}
+
+	init()
+
+	cat := func(yield func(T) bool) {
+		defer func() {
+			stop = true
+		}()
+		var positions []int
+		// var lastItem T
+		lastPos := -1
+		looped := false
+	End:
+		for !looped || len(positions) > 0 {
+			looped = true
+			positions = collectionz.Keys(*nextItems)
+			sort.Ints(positions)
+			selectedPos := -1
+			var selectedItem T
+			// fmt.Printf("nextItems: %v ; selectedPos: %d ; lastPos: %d\n", *nextItems, selectedPos, lastPos)
+			for _, pos := range positions {
+				item, ok := (*nextItems)[pos]
+				if !ok {
+					// no more entries in chan
+					delete(*nextItems, pos)
+					selectedPos = -1
+					// fmt.Printf("chan %d closed\n", pos)
+					continue
+				}
+
+				if selectedPos == -1 {
+					// Select first by default
+					// c := compare(lastItem, item)
+					selectedPos = pos
+					selectedItem = item
+					continue
+				}
+
+				// Select next position to push items
+				c := compare(item, selectedItem)
+				// fmt.Printf("compared %v to %v => score: %d\n", selectedItem, item, c)
+				if c < 0 {
+					// Prioritaire
+					selectedPos = pos
+					selectedItem = item
+					// fmt.Printf("selected pos: #%d\n", pos)
+				} else if c == 0 {
+					// Same score keep last position in priority
+					if pos == lastPos {
+						// Keep last position
+						selectedPos = pos
+						selectedItem = item
+						// fmt.Printf("selected equal pos: #%d\n", pos)
+					}
+				} else {
+					// v > 0
+					// // Need to do something ?
+				}
+			}
+
+			if selectedPos == -1 {
+				// Nothing selected
+				continue
+			}
+
+			// fmt.Printf("selectedItem: %v\n", selectedItem)
+			if !yield(selectedItem) {
+				break End
+			}
+			delete(*nextItems, selectedPos)
+			lastPos = selectedPos
+
+			mutex.Lock()
+			c, ok := (*chans)[selectedPos]
+			mutex.Unlock()
+			if ok {
+				// Consume next item of selected chan
+				e, ok := <-c
+				if ok {
+					(*nextItems)[selectedPos] = e
+				} else {
+					mutex.Lock()
+					delete(*chans, selectedPos)
+					mutex.Unlock()
+				}
+			}
+		}
+	}
+
+	return cat
+}
+
+type Entry[K, V any] struct {
+	K K
+	V V
+}
+
+func Merge2[K, V any](compare func(ka, kb K, va, vb V) int, its ...iter.Seq2[K, V]) iter.Seq2[K, V] {
+	stop := false
+	chansVal := make(map[int]chan Entry[K, V])
+	chans := &chansVal
+	nextItemsVal := make(map[int]Entry[K, V])
+	nextItems := &nextItemsVal
+	mutex := &sync.Mutex{}
+
+	init := func() {
+		stop = false
+		// m := &sync.Mutex{}
+
+		// Launch goroutines to stream produced datas
+		for pos, it := range its {
+			// Make a buffered chan of size 1
+			c := make(chan Entry[K, V], 1)
+			mutex.Lock()
+			(*chans)[pos] = c
+			mutex.Unlock()
+			go func() {
+				// Continuously push next data
+				defer close(c)
+				for k, v := range it {
+					c <- Entry[K, V]{k, v}
+					if stop {
+						break
+					}
+				}
+			}()
+		}
+
+		// Init consume first item of each chan
+		for pos, c := range *chans {
+			e, ok := <-c
+			if ok {
+				(*nextItems)[pos] = e
+			} else {
+				mutex.Lock()
+				delete(*chans, pos)
+				mutex.Unlock()
+			}
+		}
+
+		// fmt.Printf("cat paginer initialized chans: %d\n", len(*chans))
+	}
+
+	init()
+
+	cat := func(yield func(K, V) bool) {
+		defer func() {
+			stop = true
+		}()
+		var positions []int
+		// var lastItem T
+		lastPos := -1
+		looped := false
+	End:
+		for !looped || len(positions) > 0 {
+			looped = true
+			positions = collectionz.Keys(*nextItems)
+			sort.Ints(positions)
+			selectedPos := -1
+			var selectedItem Entry[K, V]
+			// fmt.Printf("nextItems: %v ; selectedPos: %d ; lastPos: %d\n", *nextItems, selectedPos, lastPos)
+			for _, pos := range positions {
+				item, ok := (*nextItems)[pos]
+				if !ok {
+					// no more entries in chan
+					delete(*nextItems, pos)
+					selectedPos = -1
+					// fmt.Printf("chan %d closed\n", pos)
+					continue
+				}
+
+				if selectedPos == -1 {
+					// Select first by default
+					// c := compare(lastItem, item)
+					selectedPos = pos
+					selectedItem = item
+					continue
+				}
+
+				// Select next position to push items
+				c := compare(item.K, selectedItem.K, item.V, selectedItem.V)
+				// fmt.Printf("compared %v to %v => score: %d\n", selectedItem, item, c)
+				if c < 0 {
+					// Prioritaire
+					selectedPos = pos
+					selectedItem = item
+					// fmt.Printf("selected pos: #%d\n", pos)
+				} else if c == 0 {
+					// Same score keep last position in priority
+					if pos == lastPos {
+						// Keep last position
+						selectedPos = pos
+						selectedItem = item
+						// fmt.Printf("selected equal pos: #%d\n", pos)
+					}
+				} else {
+					// v > 0
+					// // Need to do something ?
+				}
+			}
+
+			if selectedPos == -1 {
+				// Nothing selected
+				continue
+			}
+
+			// fmt.Printf("selectedItem: %v\n", selectedItem)
+			if !yield(selectedItem.K, selectedItem.V) {
+				break End
+			}
+			delete(*nextItems, selectedPos)
+			lastPos = selectedPos
+
+			mutex.Lock()
+			c, ok := (*chans)[selectedPos]
+			mutex.Unlock()
+			if ok {
+				// Consume next item of selected chan
+				e, ok := <-c
+				if ok {
+					(*nextItems)[selectedPos] = e
+				} else {
+					mutex.Lock()
+					delete(*chans, selectedPos)
+					mutex.Unlock()
+				}
+			}
+		}
+	}
+
+	return cat
+}
---
+++ iterz/iter_test.go
@@ -0,0 +1,71 @@
+package iterz
+
+import (
+	"cmp"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMerge_Int(t *testing.T) {
+	it1 := BuildIntSeq(1, 2, 5, 15, 12)
+	it2 := BuildIntSeq(2, 3, 13, 7)
+	it3 := BuildIntSeq(1, 4, 6, 14, 11, 8, 9)
+	expected := []int{1, 1, 2, 2, 3, 4, 5, 6, 13, 7, 14, 11, 8, 9, 15, 12}
+
+	intCompare := func(a, b int) int {
+		return cmp.Compare(a, b)
+	}
+
+	m := Merge(intCompare, it3, it1, it2)
+	var actual []int
+	for i := range m {
+		actual = append(actual, i)
+	}
+
+	assert.Equal(t, expected, actual)
+}
+
+func TestMerge_String(t *testing.T) {
+	it1 := BuildStringSeq("1a", "2a", "5a", "9a", "3a")
+	it2 := BuildStringSeq("2b", "3b", "7b", "6b")
+	it3 := BuildStringSeq("1c", "4c", "6c", "9c", "8c", "5c", "6c")
+	expected := []string{"1c", "1a", "2a", "2b", "3b", "4c", "5a", "6c", "7b", "6b", "9c", "8c", "5c", "6c", "9a", "3a"}
+
+	stringCompare := func(a, b string) int {
+		return cmp.Compare(a[0], b[0])
+	}
+
+	m := Merge(stringCompare, it3, it2, it1)
+	var actual []string
+	for i := range m {
+		actual = append(actual, i)
+	}
+
+	assert.Equal(t, expected, actual)
+}
+
+func TestMerge2_Strings(t *testing.T) {
+	itA1 := BuildStringSeq("1a", "2a", "5a", "9a", "3a")
+	itA2 := BuildStringSeq("2b", "3b", "7b", "6b")
+	itA := Link(itA1, itA2)
+	itB1 := BuildStringSeq("1c", "4c", "6c", "9c", "8c", "5c", "6c")
+	itB2 := BuildStringSeq("3d", "4d", "7d", "8d", "2d")
+	itB := Link(itB1, itB2)
+	expectedK := []string{"1c", "1a", "2a", "4c", "5a", "6c", "9c", "8c", "9a"}
+	expectedV := []string{"3d", "2b", "3b", "4d", "7b", "7d", "8d", "2d", "6b"}
+
+	stringCompare := func(a1, a2, b1, b2 string) int {
+		return cmp.Compare(a1[0], a2[0])
+	}
+	m := Merge2(stringCompare, itB, itA)
+	var actualK []string
+	var actualV []string
+	for k, v := range m {
+		actualK = append(actualK, k)
+		actualV = append(actualV, v)
+	}
+
+	assert.Equal(t, expectedK, actualK)
+	assert.Equal(t, expectedV, actualV)
+}
